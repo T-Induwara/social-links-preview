d9c7004f3be5718f85e0c0eb701d8f4eedd23be8
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-10-25 01:49:17 +0530

    Done adding all the details and basic components

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,16 +6,20 @@ import ProfileIMG from '../assets/images/avatar-jessica.jpeg';
 const Card = () => {
     return <div className="container-fluid d-flex flex-column app">
         <div className="row d-flex flex-column card">
-            <div className="detail-wrapper d-flex flex-columnn justify-content-center">
+            <div className="detail-wrapper d-flex flex-column align-items-center">
                 <img src={ProfileIMG} alt="Profile image" className="profile-img"/>
                 <h1 className="profile-name">Jessica Randall</h1>
-                <p classname="profile-location">London, United Kingdom</p>
+                <p className="profile-location">London, United Kingdom</p>
             </div>
             <div className="tagline-wrapper">
                 <p className="profile-tagline">"Front-end developer and avid reador."</p>
             </div>
-            <div className="btn-wrapper d-flex flex-column">
-
+            <div className="btn-wrapper d-flex flex-column align-items-center">
+                <button className="">GitHub</button>
+                <button className="">Frontend Mentor</button>
+                <button className="">LinkedIn</button>
+                <button className="">Twitter</button>
+                <button className="">Instagram</button>
             </div>
         </div>
     </div>
